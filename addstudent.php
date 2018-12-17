723ad79e7fe5f04ff3c9b0f180091a17a0af88c7
ymgig <daniel.morris@coderva.org>
2018-12-17 13:18:04 -0500

    yikes

--- addstudent.php
+++ addstudent.php
@@ -5,22 +5,13 @@
 <body>
   
 <form action="studentadded.php" method="post">
-  
   <b>Add a New Student</b>
-  
   <p>First Name: <input type="text" name="first_name" size="30" value="" /> </p>
-  
   <p>Last Name: <input type="text" name="last_name" size="30" value="" /> </p>
-  
-  <p><input type=submit name="submit" value="send" /> </p>
-  
-  
+  <p><input type=submit name="submit" value="send" /> </p> 
 </form>
   
 <p><a href="links.html"><b>Back to home</b></a></p>
- 
-
-
-  
+   
 </body>
 </html>
